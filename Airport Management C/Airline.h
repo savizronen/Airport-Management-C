b1f5bc8bf409d340d0b2184c555ad8c1def4a080
savizronen <72870423+savizronen@users.noreply.github.com>
2020-12-24 17:50:43 +0200

    Update Airline.h

--- Airport Management C/Airline.h
+++ Airport Management C/Airline.h
@@ -10,17 +10,17 @@ typedef struct {
 	size_t currFlightIndex;
 } Airline;
 
-Airline* initAirline(Airline* airline); // V
+Airline* initAirline(Airline* airline); 
 
-void addFlight(Airline* airline, Flight* flight); // V
+void addFlight(Airline* airline, Flight* flight); 
 
-int checkFlightsOnLine(const Airline airline, char srcPort_IATA[], char destPort_IATA[]); // V
+int checkFlightsOnLine(const Airline airline, char srcPort_IATA[], char destPort_IATA[]); 
 
 void printFlightsNumberOnAirline(const Airline airline);
 
-void freeAirline(Airline* airline); //
+void freeAirline(Airline* airline); 
 
-void printAirline(const Airline* airline); // V
+void printAirline(const Airline* airline); 
 
 #endif
 
